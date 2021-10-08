8e153f7f2296d4971eec1763c7f7588713a0c43b
helexander <jeremychee97@gmail.com>
2021-10-09 00:26:50 +1100

    Implemented axios and got fetch google books API to work

---
+++ js/index.js
@@ -0,0 +1,123 @@
+// following async await example from week 4, attempt to duplicate
+
+// google api key
+// const API_KEY = "AIzaSyBAdId07HtBnyLuS169Ja9pWVG16dRiw3Y";
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const getButton = document.getElementById("searchButton");
+
+
+const googleBooksURL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+// const getBook = async (searchInput) => {
+//     // console.log(searchInput);
+//     const response = await fetch(`${googleBooksURL}${searchInput}`);
+//     const data = await response.json();
+//     console.log(data);
+//     return data.items;
+// }
+
+const getBook = async (searchQuery) => {
+    try {
+        const { data } = await axios.get(googleBooksURL + searchQuery);
+
+        createBookCard(data.items);
+    } catch (err) {
+        createErrorCard("The book does not exist in the database.")
+    }
+}
+
+const createBookCard = (books) => {
+
+    const booksList = books.map((book) => {
+        const { volumeInfo } = book;
+
+        // populating the list with elements obtained back from the fetch
+        //         const element = document.createElement('li');
+        //         const bookText = document.createTextNode(`${book.volumeInfo.title}`);
+
+        //         element.appendChild(bookText);
+        //         return element;
+        //     });
+
+        //     const append = parent => child => parent.appendChild(child);
+        //     listItem.forEach(append(list));
+
+        const cardHTML = `
+        <div class="card">
+        <div>
+            <img
+                src="${volumeInfo.imageLinks?.thumbnail ?? ''}"
+                alt=""
+                class="avatar"
+            />
+        </div>
+        <div class="book-info">
+            <h2>${volumeInfo.title}</h2>
+            <p>
+                ${volumeInfo?.description ?? "This book has no description"}
+            </p>
+            <p>Author: ${volumeInfo.authors}</p>
+        </div>
+    </div>
+        `
+
+        return cardHTML;
+    });
+
+    main.innerHTML = booksList.join('');
+}
+
+const createErrorCard = (message) => {
+    const cardHTML = `
+    <div class="card">
+    <h1>${message}</h1>
+</div>
+    `
+
+    main.innerHTML = cardHTML;
+}
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const book = search.value;
+
+    if (book) {
+        getBook(book);
+
+        search.value = "";
+    }
+});
+
+// getButton.addEventListener("click", async (event) => {
+    //     // selecting the display block
+    //     const list = document.querySelector("#books");
+
+    //     // clearing the display block before pushing more data to it
+    //     list.innerHTML = "";
+
+    //     // selecting the search bar
+    //     const searchInput = document.querySelector("#searchInput").value;
+
+    //     // calling the getBook function to obtain/fetch data 
+    //     const booksResult = await getBook(searchInput);
+    //     console.log(booksResult);
+
+    //     // going through each object in the data returned to return its desired value
+    //     const listItem = booksResult.map((book) => {
+
+    //         // populating the list with elements obtained back from the fetch
+    //         const element = document.createElement('li');
+    //         const bookText = document.createTextNode(`${book.volumeInfo.title}`);
+
+    //         element.appendChild(bookText);
+    //         return element;
+    //     });
+
+    //     const append = parent => child => parent.appendChild(child);
+    //     listItem.forEach(append(list));
+
+// });
